76d2e039fbec40697033b9aa702d9e6596e15152
joeephwild <josephomotade0@gmail.com>
2022-07-15 07:13:42 -0700

    commit

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,7 +5,7 @@ function Movies({ movie }) {
   return (
     <div className="pl-6 relative w-[350px] transition-all duration-100 delay-300 ease-in-out hover:scale-125 ">
       <img
-        className="max-w-[300px] h-[250px] object-cover hover:border-2 rounded-br-lg rounded-tl-lg hover:border-white hover:border-bold"
+        className="max-w-[120px] h-[150px] object-cover hover:border-2 rounded-br-lg rounded-tl-lg hover:border-white hover:border-bold"
         src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
         alt={movie.title}
       />
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import { Link } from 'react-router-dom';
 import Movies from './Movies';
 
@@ -12,13 +13,38 @@ function Row({ title, rowID, fetchURL }) {
     });
   }, [fetchURL]);
 
+  const slideLeft = () => {
+    const slider = document.getElementById(`slider${rowID}`);
+    slider.scrollbarLeft = slider.scrollLeft - 500;
+  };
+
+  const slideRight = () => {
+    const slider = document.getElementById(`slider${rowID}`);
+    slider.scrollbarLeft = slider.scrollLeft + 500;
+  };
+
   return (
     <div className="mt-6">
       <h1 className="font-bold md:text-2xl text-xl m-3">{title}</h1>
-      <div className="flex tailwind-scrollbar-hide">
-        {movies.map((movie) => (
-          <Movies movie={movie} key={movie.id} />
-        ))}
+      <div className="relative items-center group">
+        <MdChevronLeft
+          onClick={slideLeft}
+          size={20}
+          className="bg-white text-blue-600 left-0 rounded-full absolute hover:opacity-100 cursor=pointer z-10 hidden group-hover:block"
+        />
+        <div
+          id={`slider${rowID}`}
+          className="flex scrollbar-hide overflow-x-scroll w-full h-full relative scroll-smooth whitespace-nowrap"
+        >
+          {movies.map((movie) => (
+            <Movies movie={movie} key={movie.id} />
+          ))}
+        </div>
+        <MdChevronRight
+          onClick={slideRight}
+          size={20}
+          className="bg-white text-blue-600 right-0 rounded-full absolute hover:opacity-100 cursor=pointer z-10 hidden group-hover:block"
+        />
       </div>
     </div>
   );
