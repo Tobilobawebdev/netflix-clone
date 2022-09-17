8aa7b47144dd242dc8ad9b317c098133ef87b9e1
Tobilobawebdev <omotadetobiloba@gmail.com>
2022-09-17 02:29:42 +0000

    commit

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -7,17 +7,18 @@ function MovieDetails() {
   const { id } = useParams();
 
   const getData = () => {
-    axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=8d4fbe03b6c5ab92b91776b7cc982e2d&language=en-US`).then((response) => {
+    axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=8d4fbe03b6c5ab92b91776b7cc982e2d&language=en-US&append_to_response=recommendations`).then((response) => {
       setCurrentMovie(response.data);
     });
   };
 
   useEffect(() => {
     getData();
+    getData2();
   }, []);
   console.log(currentMovie);
   return (
-    <div>
+    <div className="text-6xl">
       <img className="w-full h-[700px] object-cover" src={`https://image.tmdb.org/t/p/original${currentMovie.poster_path}`} />
     </div>
   );
